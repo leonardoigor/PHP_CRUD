85ef285827ae6f1a83f617cd6ec16bf1d8e93a41
leonardoigor <igor_mendonca@outlook.com.br>
2019-09-05 11:39:32 -0300

    init

--- views/save_task.php
+++ views/save_task.php
@@ -1,4 +1,15 @@
 <?php
+include("../config/db.php");
+
 if(isset($_POST['save_task'])){
-    echo "saving";
+    $title=$_POST['title'];
+    $description=$_POST['description'];
+    echo $title,$description;
+}
+
+
+
+function Consulta($title,$description){
+    $query = 'INSERT INT task('.$title.','.$description.')';
+    echo $query;
 }
